7bddc50d1a8bae3ad8d274a826e466e1af3ac1e5
ValeriaAriasC1102 <varias50158@ufide.ac.cr>
2025-06-28 19:06:49 -0600

    Add: Diseñar y crear la base de datos.
    Motor de base de datos SQL (MySQL).
    Definir el esquema de la base de datos con tablas para palabras, jugadores y partidas.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Ahorcado_Grupo5.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Ahorcado_Grupo5.Models.AhorcadoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Ahorcado_Grupo5.Models.AhorcadoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ models/Palabra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ahorcado_Grupo5.Models
+{
+    public class Palabra
+    {
+        [Key] // Llave primaria autoincremental
+        public int Id { get; set; }
+
+        [Required]// Campo obligatorio para la palabra
+        public string TextoPalabra { get; set; }
+
+        public bool HaSidoUsada { get; set; }
+    }
+}
